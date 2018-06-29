a78239a1c0c5c8b4745b11ee270f57ecff50f9d9
nenad.bojkovski <nenadbojkovski@gmail.com>
2018-06-29 16:51:14 +0200

    Fix keyboard input in fullscreenmode in chrome

--- lime/_backend/html5/HTML5Window.hx
+++ lime/_backend/html5/HTML5Window.hx
@@ -814,11 +814,8 @@ class HTML5Window {
 				untyped (textInput.style).pointerEvents = 'none';
 				textInput.style.zIndex = "-10000000";
 				
-				if (textInput.parentNode == null) {
 				
-					element.appendChild (textInput);
-				
-				}
+				element.appendChild (textInput);
 				
 			}
 			
